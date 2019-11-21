d954050102f99af3b90b6b403d52fdb37acbbb4c
Brendan R Borgers <brendanborgers@gmail.com>
2019-11-21 23:27:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,12 +14,8 @@ def id_generator
   {:id => 4}
 end
 
-def my_hash_creator(name, game)
+def my_hash_creator(:name, game)
   
   {name => game}
 end
 
-def read_from_hash(name, "Steve", "Tzvi")
-  {name => "Steve"}
-  
-end
